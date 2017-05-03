dc600ddae4b3c1f6b848b451ca118882f22638c3
Kevin Childs <kevinicus37@gmail.com>
2017-05-03 15:43:56 -0500

    Link to TMDb.  Embed Trailer to Movie Page. Display Pictures with search results.

---
+++ src/MvcTesting/Controllers/MovieController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TMDbLib.Client;
+using TMDbLib.Objects.General;
+using TMDbLib.Objects.Search;
+using MvcTesting.ViewModels;
+using TMDbLib.Objects.Movies;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MvcTesting.Controllers
+{
+    public class MovieController : Controller
+    {
+
+        TMDbClient client;
+
+        public MovieController()
+        {
+            client = new TMDbClient("9950b6bfd3eef8b5c9b7343ead080098");
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Search(string query)
+        {
+            List<Movie> movies = new List<Movie>();
+            SearchContainer<SearchMovie> results = client.SearchMovieAsync(query, 1).Result;
+            foreach (var result in results.Results)
+            {
+                Movie aMovie = client.GetMovieAsync(result.Id, MovieMethods.Credits | MovieMethods.Videos | MovieMethods.Images).Result;
+                movies.Add(aMovie);
+            }
+            SearchViewModel searchViewModel = new SearchViewModel(movies);
+            return View(searchViewModel);
+        }
+
+        public IActionResult ViewMovie(int Id)
+        {
+
+            Movie movie = client.GetMovieAsync(Id, MovieMethods.Credits | MovieMethods.Videos | MovieMethods.Images).Result;
+            return View(movie);
+        }
+    }
+
+    
+}
---
+++ src/MvcTesting/ViewModels/SearchViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TMDbLib.Objects.Search;
+using TMDbLib.Objects.General;
+using TMDbLib.Objects.Movies;
+
+namespace MvcTesting.ViewModels
+{
+    public class SearchViewModel
+    {
+        public List<Movie> Movies { get; set; }
+        public int Id { get; set; }
+
+        public SearchViewModel() { }
+
+        public SearchViewModel(List<Movie> movies)
+        {
+            Movies = movies;
+        }
+    }
+}
