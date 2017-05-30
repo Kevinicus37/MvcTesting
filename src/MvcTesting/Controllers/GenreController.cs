d6bc05da816e1f4247df76e26c86bdb673289bc5
Kevin Childs <kevinicus37@gmail.com>
2017-05-30 15:34:29 -0500

    Add Remove and Edit Actions to Genre Controller.  Add Genre Dropdown to _Layout.

--- src/MvcTesting/Controllers/GenreController.cs
+++ src/MvcTesting/Controllers/GenreController.cs
@@ -1,11 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using MvcTesting.Models;
 using MvcTesting.ViewModels;
+using System.Collections.Generic;
+using System.Linq;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace MvcTesting.Controllers
 {
+    [Authorize(Roles="Admin")]
     public class GenreController : Controller
     { 
 
@@ -19,7 +23,9 @@ namespace MvcTesting.Controllers
     // GET: /<controller>/
     public IActionResult Index()
         {
-            return View();
+            List<Genre> genres = context.Genres.ToList();
+            var vm = new GenreIndexViewModel() { Items = genres };
+            return View(vm);
         }
 
         public IActionResult Add()
@@ -39,6 +45,52 @@ namespace MvcTesting.Controllers
                 return View();
             }
             return View(addGenreViewModel);
+        }
+
+        public IActionResult RemoveGenre()
+        {
+            List<Genre> genres = context.Genres.ToList();
+            RemoveGenreViewModel vm = new RemoveGenreViewModel() { Items = genres };
+            return View(vm);
+        }
+
+        [HttpPost]
+        public IActionResult RemoveGenre(int[] genres)
+        {
+            foreach (int genre in genres)
+            {
+                Genre removeGenre = context.Genres.SingleOrDefault(g => g.ID == genre);
+                context.Genres.Remove(removeGenre);
+                context.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            Genre editGenre = context.Genres.SingleOrDefault(g => g.ID == id);
+            if (editGenre != null)
+            {
+                EditGenreViewModel vm = new EditGenreViewModel() { ID = id, Name = editGenre.Name };
+                return View(vm);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Edit(EditMovieViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+                Genre editGenre = context.Genres.SingleOrDefault(g => g.ID == vm.ID);
+                editGenre.Name = vm.Name;
+                context.SaveChanges();
+
+            }
+
+
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ src/MvcTesting/ViewModels/RemoveGenreViewModel.cs
@@ -0,0 +1,17 @@
+﻿using MvcTesting.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcTesting.ViewModels
+{
+    public class RemoveGenreViewModel : GenreViewModel
+    {
+        
+        public RemoveGenreViewModel()
+        {
+            Action = "RemoveGenre";
+        }
+    }
+}
